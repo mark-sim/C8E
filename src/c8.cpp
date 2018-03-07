e55ea28d003ed4714b65165b48537a28a7fbdd90
Sung Hoon Sim <shsim@edu.uwaterloo.ca>
2018-03-07 14:38:54 -0500

    Fixed switch statements syntax

--- src/c8.cpp
+++ src/c8.cpp
@@ -117,103 +117,117 @@ void c8::emulateCycle() {
        We first look at first 4 bits */
     switch(opcode & 0xF000) {
 
-        case 0x0000:    /* if the first 4 bits is 0 */
-            switch(opcode & 0x000F) {   /* look at the last 4 bits */
+        case 0x0000: {      /* if the first 4 bits is 0 */
+            switch (opcode & 0x000F) {   /* look at the last 4 bits */
                 case 0x0000:        /* 0X00E0: clears the screen */
-                    for(int i=0; i<2048; i++) {
+                    for (int i = 0; i < 2048; i++) {
                         gfx[i] = 0;
                     }
                     drawFlag = true;
                     pc += 2;
-                break;
+                    break;
 
                 case 0x000E:        /* 0X00EE: return from subroutine */
                     pc = stack[--sp];
                     pc += 2;
-                break;
+                    break;
 
                 default:
                     /* print opcode in hexadecimal */
                     printf("Unknown opcode 0x%X\n", opcode);
             }
-        break;
+            break;
+        }
 
-        case 0x1000:    /* if the first 4 bits is 1, 0x1NNN: Jumps to address NNN */
+        case 0x1000: {   /* if the first 4 bits is 1, 0x1NNN: Jumps to address NNN */
             pc = opcode & 0x0FFF;
-        break;
+            break;
+        }
 
-        case 0x2000:    /* if the first 4 bits is 2, 0x2NNN: Calls subroutine at NNN */
+
+        case 0x2000: {    /* if the first 4 bits is 2, 0x2NNN: Calls subroutine at NNN */
             stack[sp++] = pc;
             pc = opcode & 0X0FFF;
-        break;
+            break;
+        }
 
-        case 0x3000:    /* if the first 4 bits is 3, 0x3XNN: skips next instr if VX = NN */
-            if(V[(opcode & 0x0F00) >> 8] == opcode & 0x00FF) {
+        case 0x3000: {    /* if the first 4 bits is 3, 0x3XNN: skips next instr if VX = NN */
+            if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) {
                 pc += 4;    /* if true skip next instr */
             } else {
                 pc += 2;    /* otherwise, go to next instr */
             }
-        break;
+            break;
+        }
 
-        case 0x4000:    /* if the first 4 bits is 4, 0x4XNN: skips next instr if VX != NN */
-            if(V[(opcode & 0x0F00) >> 8] != opcode & 0x00FF) {
+        case 0x4000: {    /* if the first 4 bits is 4, 0x4XNN: skips next instr if VX != NN */
+            if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
                 pc += 4;    /* if true skip next instr */
             } else {
                 pc += 2;    /* otherwise, go to next instr */
             }
-        break;
+            break;
+        }
 
-        case 0x5000:    /* if the first 4 bits is 5, 0x5XY0: skips next instr if VX == VY */
-            if(V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4]) {
+        case 0x5000: {   /* if the first 4 bits is 5, 0x5XY0: skips next instr if VX == VY */
+            if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4]) {
                 pc += 4;    /* if true skip next instr */
             } else {
                 pc += 2;    /* otherwise, go to next instr */
             }
-        break;
+            break;
+        }
 
-        case 0x6000:    /* if the first 4 bits is 6, 0x6XNN: sets VX to NN */
+        case 0x6000: {   /* if the first 4 bits is 6, 0x6XNN: sets VX to NN */
             V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF);
             pc += 2;
-        break;
+            break;
+        }
 
-        case 0x7000:    /* if the first 4 bits is 7, 0x7XNN: Adds NN to VX */
+        case 0x7000: {    /* if the first 4 bits is 7, 0x7XNN: Adds NN to VX */
             V[(opcode & 0x0F00) >> 8] += (opcode & 0x00FF);
             pc += 2;
-        break;
+            break;
+        }
 
-        case 0x8000:    /* if the first 4 bits is 8, we have to see the last 4 bits */
-            switch(opcode & 0x000F) {
-                case 0x0000:    /* 0x8XY0: sets VX to the value of VY */
+        case 0x8000: {    /* if the first 4 bits is 8, we have to see the last 4 bits */
+            switch (opcode & 0x000F) {
+                case 0x0000: {    /* 0x8XY0: sets VX to the value of VY */
                     V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
                     pc += 2;
-                break;
+                    break;
+                }
 
-                case 0x0001:    /* 0x8XY1: sets VX to (VX or VY) */
+                case 0x0001: {    /* 0x8XY1: sets VX to (VX or VY) */
                     V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4];
                     pc += 2;
-                break;
+                    break;
+                }
 
-                case 0x0002:    /* 0x8XY2: sets VX to (VX and VY) */
+                case 0x0002: {    /* 0x8XY2: sets VX to (VX and VY) */
                     V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] & V[(opcode & 0x00F0) >> 4];
                     pc += 2;
-                break;
+                    break;
+                }
 
-                case 0x0003:    /* 0x8XY3: sets VX to (VX xor VY) */
+                case 0x0003: {    /* 0x8XY3: sets VX to (VX xor VY) */
                     V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4];
                     pc += 2;
-                break;
+                    break;
+                }
 
-                case 0x0004:    /* 0x8XY4: VX = VX + VY. VF set to 1 when there's a carry. 0 Otherwise */
-                    if(V[(opcode & 0x0F00) >> 8] > (0xFF - V[(opcode & 0x00F0) >> 4])) {     /* VX > 255 - VY */
+                case 0x0004: {    /* 0x8XY4: VX = VX + VY. VF set to 1 when there's a carry. 0 Otherwise */
+                    if (V[(opcode & 0x0F00) >> 8] > (0xFF - V[(opcode & 0x00F0) >> 4])) {     /* VX > 255 - VY */
                         V[0xF] = 1; /* carry flag, aka: the 9th bit */
                     } else {
                         V[0xF] = 0;
                     }
                     V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
                     pc += 2;
-                break;
+                    break;
+                }
 
-                case 0x0005:    /* 0x8XY5: VX = VX - VY. VF set to 0 when there's a borrow. 1 Otherwise */
+                case 0x0005: {    /* 0x8XY5: VX = VX - VY. VF set to 0 when there's a borrow. 1 Otherwise */
                     if (V[(opcode & 0x00F0) >> 4] > V[(opcode & 0x0F00) >> 8]) {        /* VY > VX */
                         V[0xF] = 0;
                     } else {
@@ -221,16 +235,18 @@ void c8::emulateCycle() {
                     }
                     V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
                     pc += 2;
-                break;
+                    break;
+                }
 
-                case 0x0006:    /* 0x8XY6: Set VX to VX shift right by 1.
+                case 0x0006: {    /* 0x8XY6: Set VX to VX shift right by 1.
  *                                 VF set to least sig bit of VX before the shift */
                     V[0xF] = V[(opcode & 0x0F00) >> 8] & 0x1;   /* this is 0 or 1 */
                     V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] >> 1;
                     pc += 2;
-                break;
+                    break;
+                }
 
-                case 0x0007:    /* 0x8XY7: VX = VY - VX. VF set to 0 when there's a borrow. 1 otherwise. */
+                case 0x0007: {    /* 0x8XY7: VX = VY - VX. VF set to 0 when there's a borrow. 1 otherwise. */
                     if (V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4]) {        /* VY > VX */
                         V[0xF] = 0;
                     } else {
@@ -238,44 +254,51 @@ void c8::emulateCycle() {
                     }
                     V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
                     pc += 2;
-                break;
+                    break;
+                }
 
-                case 0x000E:    /* 0x8XYE: Set VX to VX shift left by 1.
+                case 0x000E: {    /* 0x8XYE: Set VX to VX shift left by 1.
  *                                         VF is set to the most sig bit of VX before the shift */
                     V[0xF] = V[(opcode & 0x0F00) >> 8] >> 7;
                     V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] << 1;
                     pc += 2;
-                break;
+                    break;
+                }
 
                 default:
                     /* print opcode in hexadecimal */
                     printf("Unknown opcode: 0x%X\n", opcode);
             }
-        break;
+            break;
+        }
 
-        case 0x9000:    /* if the first 4 bits is 9, 9XY0: skip next instr if VX != VY. */
-            if(V[(opcode & 0x0F00) >> 8] !=  V[(opcode & 0x00F0) >> 4]) {
+        case 0x9000: {    /* if the first 4 bits is 9, 9XY0: skip next instr if VX != VY. */
+            if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4]) {
                 pc += 4;
             } else {
                 pc += 2;
             }
-        break;
+            break;
+        }
 
-        case 0xA000:    /* if the first 4 bits is A, 0xANNN: set I=NNN */
+        case 0xA000: {    /* if the first 4 bits is A, 0xANNN: set I=NNN */
             I = opcode & 0x0FFF;    /* the bit operation results in 12 bits */
             pc += 2;
-        break;
+            break;
+        }
 
-        case 0xB000:    /* if the first 4 bits is B, 0xBNNN: set PC = V0 + NNN */
+        case 0xB000: {    /* if the first 4 bits is B, 0xBNNN: set PC = V0 + NNN */
             pc = V[0] + (opcode & 0x0FFF);  /* 8 bits addition with 12 bits fit into 16 bits */
-        break;
+            break;
+        }
 
-        case 0xC000:    /* if the first 4 bits is C, 0xCXNN: set VX = rand() & NN. rand() should be 0-255 (8bits). */
+        case 0xC000: {    /* if the first 4 bits is C, 0xCXNN: set VX = rand() & NN. rand() should be 0-255 (8bits). */
             V[(opcode & 0x0F00) >> 8] = (rand() % 256) & (opcode & 0x00FF); /* 0-255 is 8 bits AND with 8 bits */
             pc += 2;
-        break;
+            break;
+        }
 
-        case 0xD000:    /* if the first 4 bits is D, 0xDXYN: draw a sprite at (VX,VY).
+        case 0xD000: {    /* if the first 4 bits is D, 0xDXYN: draw a sprite at (VX,VY).
                            Should be 8 pixels wide and N pixels high.
                            I should be pointing to the base sprite address we want to draw */
             unsigned short x = V[(opcode & 0x0F00) >> 8];   /* TODO: maybe try using unsigned char for all? */
@@ -284,11 +307,11 @@ void c8::emulateCycle() {
             unsigned short pixel;
 
             V[0xF] = 0;
-            for(int i=0; i<height; i++) {
+            for (int i = 0; i < height; i++) {
                 pixel = memory[I + i];
-                for(int j=0; j<8; j++) {
-                    if((pixel & (0x80 >> j)) != 0) {    /* if sprite pixel bit we want to draw is 1 */
-                        if(gfx[x + j + ((y + i) * 64)] == 1) {   /* AND the pixel bit at the screen is 1 */
+                for (int j = 0; j < 8; j++) {
+                    if ((pixel & (0x80 >> j)) != 0) {    /* if sprite pixel bit we want to draw is 1 */
+                        if (gfx[x + j + ((y + i) * 64)] == 1) {   /* AND the pixel bit at the screen is 1 */
                             V[0xF] = 1;
                         }
                         gfx[x + j + ((y + i) * 64)] = gfx[x + j + ((y + i) * 64)] ^ 1;
@@ -297,32 +320,36 @@ void c8::emulateCycle() {
             }
             drawFlag = true;
             pc += 2;
-        break;
+            break;
+        }
 
-        case 0xE000:    /* if the first 4 bits is E, need to check last 4 or 8 bits */
-            switch(opcode & 0x00FF) {
-                case 0x009E:    /* if the last 8 bits is 9E, 0xEX9E: skip next instr if key in VX is pressed */
+        case 0xE000: {   /* if the first 4 bits is E, need to check last 4 or 8 bits */
+            switch (opcode & 0x00FF) {
+                case 0x009E: {    /* if the last 8 bits is 9E, 0xEX9E: skip next instr if key in VX is pressed */
                     if (key[V[(opcode & 0x0F00) >> 8]] != 0) {
                         pc += 4;
                     } else {
                         pc += 2;
                     }
                     break;
+                }
 
-                case 0x00A1:    /* if the last 8 bits is A1, 0xEXA1: skip next instr if key in VX isn't pressed */
+                case 0x00A1: {    /* if the last 8 bits is A1, 0xEXA1: skip next instr if key in VX isn't pressed */
                     if (key[V[(opcode & 0x0F00) >> 8]] == 0) {
                         pc += 4;
                     } else {
                         pc += 2;
                     }
                     break;
+                }
 
                 default:
                     printf("Unknown opcode: 0x%X\n", opcode);
             }
-        break;
+            break;
+        }
 
-            
+
 
         default:
             /* print opcode in hexadecimal */
